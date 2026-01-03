c061b33d8bc08794c38806935880d8dd797e482c
Michael-Grimm <86737735+Michael-Grimm@users.noreply.github.com>
2026-01-03 16:47:00 +0100

    Update sha256.h

--- sha256.h
+++ sha256.h
@@ -1,7 +1,9 @@
 
 /**
+ * Implementation of the SHA-256 algorithm in ARM-64 bit assembly.
+ * 
  * message: 
- * message_length: length in bits
+ * message_length: length in bits (max 2^64 -1 bits)
  * hash: pointer to an array of eight u32 integers
  */
 void sha256(int *message, long long int message_length_in_bits, int *hash);
