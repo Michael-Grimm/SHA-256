46f27038adfcfcb983cd82b4682e9ab4a4886dd8
Michael-Grimm <86737735+Michael-Grimm@users.noreply.github.com>
2026-01-03 16:51:28 +0100

    Update sha256.h

--- sha256.h
+++ sha256.h
@@ -1,10 +1,10 @@
 
 /**
  * Implementation of the SHA-256 algorithm in ARM-64 bit assembly.
- * 
- * message: 
+ * https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf
+ * message: pointer to an array of u32 integers
  * message_length: length in bits (max 2^64 -1 bits)
- * hash: pointer to an array of eight u32 integers
+ * hash: final hash value, pointer to an array of eight u32 integers
  */
 void sha256(int *message, long long int message_length_in_bits, int *hash);
 
