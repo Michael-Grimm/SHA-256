7b52989464ca0dfadaefa6f8aef9145f16045604
Michael-Grimm <86737735+Michael-Grimm@users.noreply.github.com>
2026-01-03 16:54:05 +0100

    Update sha256tests.h

--- sha256tests.h
+++ sha256tests.h
@@ -1,5 +1,6 @@
 /**
- *  Tests functions for 4.1.2 SHA-256 functions.
+ * Unit tests for the sha256 algorithm  (message padding, preprocessing, working variables, hash functions, hash computation)
+ * Some tests use examples from https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/SHA256.pdf
  */
  int test_Sigma0( int);
  int test_Sigma1( int);
@@ -7,8 +8,8 @@
  int test_sig1( int);
  int test_Ch( int x,  int y,  int z);
  int test_Maj( int x,  int y,  int z);
- int  test_setT1( int h,  int e,  int f,  int g,  int k,  int w);
- int  test_setT2( int a,  int b,  int c);
+ int test_setT1( int h,  int e,  int f,  int g,  int k,  int w);
+ int test_setT2( int a,  int b,  int c);
 void prepare_first_16_schedules( int *block,  int *workingschedule);
 void calculate_values_from_messagelength(  int *actual,   int messagelength); 
 void clear_schedules(  int *values);
